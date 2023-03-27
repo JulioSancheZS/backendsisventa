486583966060d70524492bd131cb2bcaf0036737
Julio Sanchez <julio239995@gmail.com>
2023-03-26 18:02:40 -0600

    Configuracion a producción

--- src/db/dbconexion.ts
+++ src/db/dbconexion.ts
@@ -2,9 +2,17 @@ import {Sequelize} from 'sequelize'
 
 
 // Option 3: Passing parameters separately (other dialects)
+//LocalHost
 const sequelize = new Sequelize('sisventa', 'root', '1234', {
     host: 'localhost',
     dialect: 'mysql'
   });
+
+  //Produccion
+/*
+  const sequelize = new Sequelize('sisventa', 'root', '1234', {
+    host: 'localhost',
+    dialect: 'mysql'
+  }); */
 
   export default sequelize
